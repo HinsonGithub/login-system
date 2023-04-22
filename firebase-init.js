d69e31460db1a3322db9e54ef2fda5673a5d4f5f
Hinson Chu <98829934+HinsonGithub@users.noreply.github.com>
2023-04-22 20:50:59 +0800

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIhGCLOGOy74X9qpzIogToFhYqYJ-wnnc",
+  authDomain: "auth-548b8.firebaseapp.com",
+  projectId: "auth-548b8",
+  storageBucket: "auth-548b8.appspot.com",
+  messagingSenderId: "424765931773",
+  appId: "1:424765931773:web:4a7694bb7bb1e57d0fbe94",
+  measurementId: "G-MQTH77MNTG"
+};;
+
+firebase.initializeApp(firebaseConfig);
